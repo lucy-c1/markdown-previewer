4ebe98f3880bef817bd48ee2612dc99a31522548
LucyC-1225 <lucyc1225xx@gmail.com>
2024-08-28 19:40:06 -0400

    Resolves #4 - implemented basic functionality

--- components/Previewer.jsx
+++ components/Previewer.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 
-export default function Previewer() {
+export default function Previewer(props) {
     return (
         <div className = "previewer-container">
             <h1>Previewer:</h1>
-            {/* this is where the parsed markdown will be inserted */}
-            <div id = "preview">
-                
+            {/* this is where the parsed markdown will be inserted, turn String to HTML
+            (which is already sanitized so it is safe to use dangerouslySetInnerHTML)
+            */}
+            <div id = "preview" dangerouslySetInnerHTML={{ __html: props.parsedMarkdown }}>
             </div>
         </div>
     )
--- index.jsx
+++ index.jsx
@@ -2,19 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Editor from './components/Editor';
 import Previewer from './components/Previewer';
+import { marked } from 'marked';
+import DOMPurify from 'dompurify';
 
 function App() {
   /* what the user types into the editor */
   const [markdown, setMarkdown] = React.useState("");
+  /* markdown parsed by marked library, displays in previewer */
+  const [parsedMarkdown, setParsedMarkdown] = React.useState();
+
   function updateMarkdown(event) {
     setMarkdown(event.target.value);
+  }
+
+  React.useEffect(() => {
+    parseMarkdown();
+  }, [markdown]);
+
+  function parseMarkdown() {
+    const html = marked(markdown);
+    // make sure the html is safe using dompurify
+    const safeHtml = DOMPurify.sanitize(html);
+    console.log(safeHtml);
+    setParsedMarkdown(safeHtml);
   }
   return (
     <div>
       <Editor 
       updateMarkdown = {updateMarkdown}
       />
-      <Previewer />
+      <Previewer 
+      parsedMarkdown = {parsedMarkdown}
+      />
     </div>
   )
 }
